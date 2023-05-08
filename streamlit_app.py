635e7f906305be579b978696f65b26a430463cbb
Nareshreddy Kola <kola.nareshreddy@gmail.com>
2023-05-08 06:32:20 -0400

    Adding images to breakfast menu list

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,9 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Dinner')
 
-streamlit.header('Breakfast Menu')
+streamlit.header('Breakfast Favorites')
 
-streamlit.text('Omega 3 & Blueberry Oat Meal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-boiled Free-range eggs')
+streamlit.text('🥣 Omega 3 & Blueberry Oat Meal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-boiled Free-range eggs')
+streamlit.text('🥑🍞 Avocado Toast')
