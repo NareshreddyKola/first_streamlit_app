08fef1d995242d851f00665579df8e072ebfd38f
Nareshreddy Kola <kola.nareshreddy@gmail.com>
2023-05-08 06:55:40 -0400

    Adding a default option for customer reference

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,6 +16,6 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 myfruit_list = pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
 myfruit_list = myfruit_list.set_index('Fruit')
 
-streamlit.multiselect('Pick Some Fruits', list(myfruit_list.index))
+streamlit.multiselect('Pick Some Fruits', list(myfruit_list.index), ['Avocado','Strawberries'])
 
 streamlit.dataframe(myfruit_list)
