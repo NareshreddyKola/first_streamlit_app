796a711eb1df9be4308cfabd25fe28c78af79f66
Nareshreddy Kola <kola.nareshreddy@gmail.com>
2023-05-08 06:28:48 -0400

    correcting title to text for breakfast menu items

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,6 @@ streamlit.title('My Parents New Healthy Dinner')
 
 streamlit.header('Breakfast Menu')
 
-streamlit.title('Omega 3 & Blueberry Oat Meal')
-streamlit.title('Kale, Spinach & Rocket Smoothie')
-streamlit.title('Hard-boiled Free-range eggs')
+streamlit.text('Omega 3 & Blueberry Oat Meal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-boiled Free-range eggs')
