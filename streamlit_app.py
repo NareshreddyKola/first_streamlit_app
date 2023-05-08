9555cbe79790b84f7b13d177a3742fd01fbacbfa
Nareshreddy Kola <kola.nareshreddy@gmail.com>
2023-05-08 06:27:46 -0400

    Breakfast menu changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Dinner')
+
+streamlit.header('Breakfast Menu')
+
+streamlit.title('Omega 3 & Blueberry Oat Meal')
+streamlit.title('Kale, Spinach & Rocket Smoothie')
+streamlit.title('Hard-boiled Free-range eggs')
