fbffb07bf3d5db2ca82ab744682dbc90c7ff3ad1
Nareshreddy Kola <kola.nareshreddy@gmail.com>
2023-05-09 21:29:11 -0400

    Introduced user input text field for fruit name

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,7 +22,9 @@ fruits_show = myfruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_show)
 
 streamlit.header('Fruityvice Fruit Advice!')
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+fruit_choice = streamlit.text_input('What Fruit information would you like information about?', 'kiwi')
+streamlit.write('The Customer Entered', fruit_choice)
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 #streamlit.text(fruityvice_response.json())
 
 #normalize json response
