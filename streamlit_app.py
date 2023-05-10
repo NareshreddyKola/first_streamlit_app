2436f53b4edf553bd21eda26707e34b399e7ddf8
Nareshreddy Kola <kola.nareshreddy@gmail.com>
2023-05-09 21:24:43 -0400

    Code refactoring to separate fruit name

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,11 +22,12 @@ fruits_show = myfruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_show)
 
 streamlit.header('Fruityvice Fruit Advice!')
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+#streamlit.text(fruityvice_response.json())
 
 #normalize json response
 fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
 #Render normalized fruityvice to a streamlit dataframe 
 streamlit.dataframe(fruityvice_normalized)
+
 
