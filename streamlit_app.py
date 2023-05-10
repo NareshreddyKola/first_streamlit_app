9aac05c056905908c47bf5c645f5b22ec231c6f2
Nareshreddy Kola <kola.nareshreddy@gmail.com>
2023-05-10 06:29:03 -0400

    another text input to add fruit

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,3 +43,6 @@ my_cur.execute("select * from fruit_load_list")
 my_data_rows = my_cur.fetchall()
 streamlit.header("My Fruit Load List contains:")
 streamlit.dataframe(my_data_rows)
+
+add_my_fruit = streamlit.text_input('What Fruit would you like to add?')
+streamlit.write('The Customer Entered', add_my_fruit)
